5527118307847a5f17ed337ae340c4a1e3cb43d5
TERRENCE LAI <terrencelai@TERRENCEs-MacBook-Air.local>
2020-04-06 18:08:50 -0400

    fix category_associations

--- app/models/category_association.rb
+++ app/models/category_association.rb
@@ -1,15 +1,15 @@
 class CategoryAssociation < ApplicationController
 
-    belongs_to :projects,
-    primary_key: :id,
-    foreign_key: :project_id,
-    class_name: 'Project',
-    required: false
+    # belongs_to :projects,
+    # primary_key: :id,
+    # foreign_key: :project_id,
+    # class_name: 'Project',
+    # required: false
 
-    belongs_to :categories,
-    primary_key: :id,
-    foreign_key: :category_id,
-    class_name: 'Category'
+    # belongs_to :categories,
+    # primary_key: :id,
+    # foreign_key: :category_id,
+    # class_name: 'Category'
     
 
 end
