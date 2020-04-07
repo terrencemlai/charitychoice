22b72a50d3eb4d9a0a88b03a4c8f7b3a70cf2f99
TERRENCE LAI <terrencelai@TERRENCEs-MacBook-Air.local>
2020-04-07 10:32:10 -0400

    additional setup

---
+++ frontend/main.jsx
@@ -0,0 +1,9 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Coming Soon</h1>, root);
+});
