c9ec3edd21d123592f1158e6091fad10e9a892ac
TERRENCE LAI <terrencelai@TERRENCEs-MacBook-Air.local>
2020-04-10 13:17:54 -0400

    modal

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,12 +1,15 @@
 import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
-  
+import { CLOSE_MODAL } from '../actions/modal_actions';
+
   export default (oldState = [], action) => {
     Object.freeze(oldState);
-
+    debugger;
     switch (action.type) {
       case RECEIVE_SESSION_ERRORS:
         return action.errors;
       case RECEIVE_CURRENT_USER:
+        return [];
+      case CLOSE_MODAL:
         return [];
       default:
         return oldState;
