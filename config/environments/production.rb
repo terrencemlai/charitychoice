31156bb48b76b09d7f682130d93ba95451f0290b
TERRENCE LAI <terrencelai@TERRENCEs-MacBook-Air.local>
2020-04-11 18:23:54 -0400

    image issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.assets.compile = true
-  
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -30,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
