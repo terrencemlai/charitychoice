7869b24d7ddbc7f7e6260c4f70c822e5db4d174f
TERRENCE LAI <terrencelai@TERRENCEs-MacBook-Air.local>
2020-04-11 17:50:35 -0400

    heroku image issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.serve_static_files = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.assets.compile = true
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
