ea41d3a5ca166c9ddda59fb43a1ca45c766310e6
TERRENCE LAI <terrencelai@TERRENCEs-MacBook-Air.local>
2020-04-12 18:13:54 -0400

    responsive enchancements

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,40 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Splash extends React.Component {
     constructor(props){
         super(props);
     }
 
-    render(){
+    renderWelcome(){
+        return (
+            <div id="welcome-message">
+                <h1>Make a difference.</h1>
+                <h1>Give back.</h1>
+                <p className="subheader">
+                    Teachers and students need your help to transform their classrooms and maximize their educational enrichment. 
+                    Fund supplies, equipment, class trips, and more for a classroom project of your choice today.
+                </p>
+                <Link className="link" to="/"><div>See classroom projects</div></Link>
+                <p className="subblurb">
+                    Thank you for considering this demonstration site, inspired by DonorsChoose.org
+                </p>
+            </div>
+        )
 
+    }
+
+    render(){
         return (
             <div className="splash-main">
-                <div className="splash-row-01">
+                <div className="splash-row splash-row-01">
                     <span className="splash-row-01-A">
-                        <span className="splash-row-01-B">WELCOME MESSAGE</span>
+                        <span className="splash-row-01-B">{this.renderWelcome()}</span>
                     </span>
+                    <span className="splash-row-01-C">{this.renderWelcome()}</span>
                 </div>
-                <div className="splash-row-01-c">WELCOME MESSAGE</div>
 
-                <div className="splash-row splash02">
+                <div className="splash-row splash-row-02">
                     SEARCH BAR
                 </div>
-                <div className="splash-row splash03">
-                    TEACHER GREETING
+                <div className="splash-row splash-row-03">
+                        <span>Are you a school teacher?</span>
+                        <Link className="link" to="/teachers/signup"><div>Register to create projects</div></Link>
                 </div>
-                <div className="splash-row splash04">
+                <div className="splash-row splash-row-04">
                     NEARBY PROJECTS
                 </div>
-                <div className="splash-row splash05">
+                <div className="splash-row splash-row-05">
                     ABOUT US
                 </div>
-                <div className="splash-row splash06">
+                <div className="splash-row splash-row-06">
                     MORE INFO
                 </div>
-                <div className="splash-row splash07">
+                <div className="splash-row splash-row-07">
                     YET MORE INFO
                 </div>
-                <div className="splash-row splash08">
+                <div className="splash-row splash-row-08">
                     EVEN MORE INFO
                 </div>
             </div>
